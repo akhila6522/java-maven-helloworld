d76413f5c44e08df7134a1a70584304e8c0b4d9d
TestGit <vasyaalive@yahoo.com>
2023-05-05 18:22:40 +0300

    One test will ignored

--- src/test/java/hello/GreeterTest.java
+++ src/test/java/hello/GreeterTest.java
@@ -4,6 +4,7 @@ import static org.hamcrest.core.StringContains.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Disabled;
 
 public class GreeterTest {
     private Greeter greeter = new Greeter();
@@ -13,6 +14,7 @@ public class GreeterTest {
 	    assertThat(greeter.SayHello(), containsString("Hello"));
     }
 
+    @Disabled("Must be ignored")
     @Test
     public void greeterSaysHello2() {
         assertThat(greeter.SayHello(), containsString("World"));
